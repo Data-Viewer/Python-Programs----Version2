75de0283498fedb851ee5f513136c078a832879e
Shalini J <99327803+Data-Viewer@users.noreply.github.com>
2022-02-21 19:00:58 +0530

    Add files via upload

---
+++ separate key value pair with arrow mark in dictionary.py
@@ -0,0 +1,6 @@
+def dict():
+    print("dictonary:")
+    dict = {"course1":"python","course2":"datascience","course3":'java',"course4":"django"}
+    for i,j in dict.items():
+        print(i,"-->",j)
+dict()
